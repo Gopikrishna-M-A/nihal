6c3c4f21c1550574bbe13adf2e0cb3030e7ab2ae
GOPIKRISHNA-M-A <gopikrishna6003@gmail.com>
2023-12-09 17:02:34 +0530

    reduced image sizes

--- components/Materials.jsx
+++ components/Materials.jsx
@@ -7,31 +7,31 @@ const Materials = () => {
         <div className="materials">
             <div className="wood">
                 <div className="wood-text">TEAK</div>
-                <img src="/images/maple.jpg" alt="" />
+                <img src="/images/w1.png" alt="" />
             </div>
             <div className="wood">
                 <div className="wood-text">MAHOGANY</div>
-                <img src="/images/chery.jpg" alt="" />
+                <img src="/images/w2.png" alt="" />
 
             </div>
             <div className="wood">
                 <div className="wood-text">ROSEWOOD</div>
-                <img src="/images/walnut.jpg" alt="" />
+                <img src="/images/w6.png" alt="" />
 
             </div>
             <div className="wood">
                 <div className="wood-text">NEEM</div>
-                <img src="/images/w2.avif" alt="" />
+                <img src="/images/w4.png" alt="" />
 
             </div>
             <div className="wood">
                 <div className="wood-text">MAPLE</div>
-                <img src="/images/w1.jpg" alt="" />
+                <img src="/images/w5.png" alt="" />
 
             </div>
             <div className="wood">
                 <div className="wood-text">OAK</div>
-                <img src="/images/w3.jpg" alt="" />
+                <img src="/images/w3.png" alt="" />
                 
             </div>
         </div>
