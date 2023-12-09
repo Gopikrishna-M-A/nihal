8131909de68276eda52e76187df4b3d299331e3c
GOPIKRISHNA-M-A <gopikrishna6003@gmail.com>
2023-12-09 17:34:10 +0530

    updated footer year

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,10 @@
 import { Button } from 'antd';
 import { InstagramOutlined, FacebookOutlined, TwitterOutlined } from '@ant-design/icons';
 
+const getFullYear = () => {
+  return new Date().getFullYear();
+};
+
 const Footer = () => {
   return (
     <div className='footer'>  
@@ -10,7 +14,7 @@ const Footer = () => {
             <div className="social-link"><Button icon={<FacebookOutlined />} type='link'></Button></div>
             <div className="social-link"><Button icon={<TwitterOutlined />} type='link'></Button></div>
         </div>
-        <div className="copy-rights">© 2016 KNM Timbers . All Rights Reserved.</div>
+        <div className="copy-rights">©  {getFullYear()}  KNM Timbers . All Rights Reserved.</div>
         <img src="/images/texture.png" alt="" className="footer-bg" />
     </div>
   )
